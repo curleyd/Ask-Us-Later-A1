1c1784bcd4f89b96c665c84273adcf010e462f8b
Will-Bahnmiller <will.bahnmiller@gmail.com>
2015-02-12 23:02:19 -0500

    Controller works with car-like controls.
    Script in place to initiate agents via left and right-clicks.
    
    Images are placeholder for now.

---
+++ MyCommit/Assets/scripts/PlayerAgentController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAgentController : MonoBehaviour {
+	
+	public int myID;
+	public float turnSpeed;
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		myID = -1;
+		turnSpeed = 100f;
+		moveSpeed = 5f;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		// Check for key presses
+		checkForInput ();
+
+	} // end of Update()
+
+
+	// Check for valid key presses (W,S,A,D) and move or turn accordingly
+	void checkForInput() {
+
+		// this is how to move
+		//transform.Translate(+/- Vector3.up * moveSpeed * Time.deltaTime);
+		
+		// Check for input: W = Forwards
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+		}
+		
+		// Check for input: S = Backwards
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime);
+		}
+
+		// this is how to rotate in 2D
+		//transform.Rotate(Vector3.forward, +/- turnSpeed * Time.deltaTime);
+		
+		// Check for input: A = Turn left
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+		}
+		
+		// Check for input: D = Turn right
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+		}
+	
+	} // end of checkForInput()
+}
