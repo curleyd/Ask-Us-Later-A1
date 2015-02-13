b9adc4619aa14559af124f8273c89091a5452a99
Will-Bahnmiller <will.bahnmiller@gmail.com>
2015-02-13 17:00:13 -0500

    Meeting 2, Commit 2.
    Ray sensor and walls (no collision).

---
+++ MyCommit2/Assets/scripts/RaySensor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RaySensor : MonoBehaviour {
+
+	public float maxDistance = 5f;
+
+	// Update is called once per frame
+	void Update () {
+
+		collider2D.enabled = false;
+		RaycastHit2D hitUp = Physics2D.Raycast(transform.position, transform.up, maxDistance);
+		RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, transform.up + -1 * transform.right, maxDistance);
+		RaycastHit2D hitRight = Physics2D.Raycast(transform.position, transform.up + transform.right, maxDistance);
+
+		if (hitUp.collider != null || hitLeft.collider != null || hitRight.collider != null) {
+			Debug.DrawRay(transform.position, transform.up, Color.green);
+			Debug.Log("HitUp = " + hitUp.distance + " " + "HitLeft = " + hitLeft.distance + " " + "HitRight = " + hitRight.distance);
+		}
+
+		collider2D.enabled = true;
+
+
+	}
+}
