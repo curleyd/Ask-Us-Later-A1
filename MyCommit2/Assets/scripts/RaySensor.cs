72dc0e5a58e2b1718f3cedfb769e3538b0a703b6
joshhirsch <cowboys500@gmail.com>
2015-02-15 13:41:10 -0500

    Collisions
    Subject now collides with wall and won't go outside them/

--- MyCommit2/Assets/scripts/RaySensor.cs
+++ MyCommit2/Assets/scripts/RaySensor.cs
@@ -16,7 +16,13 @@ public class RaySensor : MonoBehaviour {
 		if (hitUp.collider != null || hitLeft.collider != null || hitRight.collider != null) {
 			Debug.DrawRay(transform.position, transform.up, Color.green);
 			Debug.Log("HitUp = " + hitUp.distance + " " + "HitLeft = " + hitLeft.distance + " " + "HitRight = " + hitRight.distance);
+
 		}
+
+		Debug.DrawRay(transform.position, Vector3.Normalize(transform.up)*(maxDistance+1), Color.green, 0);
+		Debug.DrawRay(transform.position, Vector3.Normalize(transform.up + -1 * transform.right)*(maxDistance+1), Color.green, 0);
+		Debug.DrawRay(transform.position, Vector3.Normalize(transform.up + transform.right)*(maxDistance+1), Color.green, 0);
+
 
 		collider2D.enabled = true;
 
