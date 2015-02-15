f699bf914413ceef7d28d1c6cd0d54446915a626
Will-Bahnmiller <will.bahnmiller@gmail.com>
2015-02-14 22:16:10 -0500

    Adjacent Agent Sensor (the circular radar) is fully working!
    All that's left is the Pie-slice.

---
+++ MyCommit3/Assets/scripts/StaticAgentController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticAgentController : MonoBehaviour {
+
+	public int myID;
+
+	// Use this for initialization
+	void Start () {
+		myID = Camera.main.GetComponent<SpawnAgent>().staticCounter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public int getID() {
+		return myID;
+	}
+}
